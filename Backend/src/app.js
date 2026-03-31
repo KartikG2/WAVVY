2dc7b2db455958fb7c566fad6b0d921877a6421d
KartikG2 <kartikgangavati2004@gmail.com>
2026-04-01 00:10:09 +0530

    commiting cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,11 @@ const Uri = process.env.MONGO_URL;
 
 // middlewares 
 app.set("port", (process.env.PORT || 8080));
-app.use(cors());
+app.use(cors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
